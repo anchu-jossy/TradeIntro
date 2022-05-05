c543ae1cd2c8803919fc45258f0234bcf4d5163f
Smriti Katiyar <smriti.katiyar@ibm.com>
2022-05-05 17:30:32 +0530

    font added

--- app/src/main/java/com/techxform/tradintro/feature_main/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/techxform/tradintro/feature_main/presentation/home/HomeFragment.kt
@@ -1,7 +1,10 @@
 package com.techxform.tradintro.feature_main.presentation.home
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.techxform.tradintro.R
@@ -23,6 +26,10 @@ class HomeFragment : BaseFragment<HomeFragmentBinding>(HomeFragmentBinding::infl
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+ /*       val face: Typeface? = ResourcesCompat.getFont(requireContext(), R.font.open_sans)
+        val searchText = binding.searchView as TextView
+        searchText.typeface = face*/
 
         binding.portfolioRv.adapter = PortfolioAdapter(arrayListOf(),rvListener)
 
