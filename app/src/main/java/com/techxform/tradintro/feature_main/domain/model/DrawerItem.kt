26f3960e25b9e5f8166a8657def14c4344e94e17
Smriti Katiyar <smriti.katiyar@ibm.com>
2022-05-06 20:04:49 +0530

    add drawer

---
+++ app/src/main/java/com/techxform/tradintro/feature_main/domain/model/DrawerItem.kt
@@ -0,0 +1,5 @@
+package com.techxform.tradintro.feature_main.domain.model
+
+import androidx.annotation.DrawableRes
+
+data class DrawerItem(val name: String, @DrawableRes val icon: Int)
--- app/src/main/java/com/techxform/tradintro/feature_main/presentation/landing/LandingFragment.kt
+++ app/src/main/java/com/techxform/tradintro/feature_main/presentation/landing/LandingFragment.kt
@@ -11,12 +11,46 @@ import androidx.navigation.ui.setupWithNavController
 import com.techxform.tradintro.R
 import com.techxform.tradintro.core.base.BaseFragment
 import com.techxform.tradintro.databinding.FragmentLandingBinding
+import com.techxform.tradintro.feature_main.domain.model.DrawerItem
 
 
 class LandingFragment : BaseFragment<FragmentLandingBinding>(FragmentLandingBinding::inflate) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        bottomNavSetup()
+        drawerSetup()
+
+
+    }
+
+    private fun drawerSetup() {
+        binding.drawerRv.adapter = DrawerAdapter(createDrawerItems())
+    }
+
+    private fun createDrawerItems(): ArrayList<DrawerItem> {
+        val list = arrayListOf<DrawerItem>()
+
+        list.add(DrawerItem(getString(R.string.my_profile_lbl), R.drawable.ic_my_profile))
+        list.add(DrawerItem(getString(R.string.my_referrals_lbl), R.drawable.my_referrals))
+        list.add(DrawerItem(getString(R.string.my_skill_board_lbl), R.drawable.my_skill_board))
+        list.add(DrawerItem(getString(R.string.recharge_summary_lbl), R.drawable.recharge_summary))
+        list.add(
+            DrawerItem(
+                getString(R.string.recharge_trade_money_lbl),
+                R.drawable.recharge_trade_money
+            )
+        )
+        list.add(DrawerItem(getString(R.string.notification_lbl), R.drawable.ic_notification))
+        list.add(DrawerItem(getString(R.string.reports_lbl), R.drawable.reports))
+        list.add(DrawerItem(getString(R.string.alerts_lbl), R.drawable.ic_alerts))
+        list.add(DrawerItem(getString(R.string.change_password_lbl), R.drawable.change_pass))
+        list.add(DrawerItem(getString(R.string.logout_lbl), R.drawable.ic_logout))
+        return list
+    }
+
+    private fun bottomNavSetup() {
         val navHostFragment =
             childFragmentManager.findFragmentById(R.id.container_fragment) as NavHostFragment
         val navController = navHostFragment.navController
@@ -59,15 +93,15 @@ class LandingFragment : BaseFragment<FragmentLandingBinding>(FragmentLandingBind
             }
     }
 
-    private fun manageBottomNavVisiblity()
-    {
+    private fun manageBottomNavVisiblity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            view?.setWindowInsetsAnimationCallback(object : WindowInsetsAnimation.Callback(DISPATCH_MODE_STOP) {
+            view?.setWindowInsetsAnimationCallback(object :
+                WindowInsetsAnimation.Callback(DISPATCH_MODE_STOP) {
                 override fun onProgress(
                     p0: WindowInsets,
                     p1: MutableList<WindowInsetsAnimation>
                 ): WindowInsets {
-                    return  p0
+                    return p0
                 }
 
                 override fun onEnd(animation: WindowInsetsAnimation) {
@@ -77,11 +111,11 @@ class LandingFragment : BaseFragment<FragmentLandingBinding>(FragmentLandingBind
                     binding.bottomNav.isVisible = !showingKeyboard
                 }
             })
-        }else {
+        } else {
             val view = requireActivity().window.decorView
             ViewCompat.setOnApplyWindowInsetsListener(view) { v, insets ->
                 val showingKeyboard = insets.isVisible(WindowInsetsCompat.Type.ime())
-                binding. bottomNav.isVisible = !showingKeyboard
+                binding.bottomNav.isVisible = !showingKeyboard
                 insets
             }
         }
